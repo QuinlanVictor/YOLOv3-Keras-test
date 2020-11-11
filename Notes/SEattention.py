026063458ad21e1b33624d14a2863361f58bc420
QuinlanVictor <qianliang1994@126.com>
2020-11-11 15:45:28 +0800

    11.11
    11.11

--- Notes/SEattention.py
+++ Notes/SEattention.py
@@ -1,4 +1,11 @@
 '''把senet加入inception，理解一下思路就好'''
+from keras.layers import GlobalAveragePooling2D, Reshape, Dense, multiply, \
+    Activation, Input, Dropout
+from keras.models import Model
+from keras import backend as K
+from keras.optimizers import Adam
+
+from keras.applications import InceptionV3
 
 def build_model(nb_classes, input_shape=(256,256,3)):
     inputs_dim = Input(input_shape)
@@ -25,8 +32,8 @@ def build_model(nb_classes, input_shape=(256,256,3)):
     model = Model(inputs=inputs_dim, outputs=fc2)
     return model
 
-if __name__ == '__main__':
-	model =build_model(nb_classes, input_shape=(im_size1, im_size2, channels))
-	opt = Adam(lr=2*1e-5)
-    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit（）
+# if __name__ == '__main__':
+	# model =build_model(nb_classes, input_shape=(im_size1, im_size2, channels))
+	# opt = Adam(lr=2*1e-5)
+    # model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    # model.fit（）
