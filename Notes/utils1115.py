fdbcb10ceb24e3536f5f48f379543c890223f6ac
QuinlanVictor <qianliang1994@126.com>
2020-11-15 23:20:28 +0800

    测试余弦退火调整学习率和warm up
    测试余弦退火调整学习率和warm up

---
+++ Notes/utils1115.py
@@ -0,0 +1,243 @@
+"""Miscellaneous utility functions.
+
+20201115 测试余弦退火
+
+"""
+
+from functools import reduce
+
+from PIL import Image
+import numpy as np
+from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
+import keras
+import keras.backend as K
+
+
+def compose(*funcs):
+    """Compose arbitrarily many functions, evaluated left to right.
+
+    Reference: https://mathieularose.com/function-composition-in-python/
+    """
+    # return lambda x: reduce(lambda v, f: f(v), funcs, x)
+    if funcs:
+        return reduce(lambda f, g: lambda *a, **kw: g(f(*a, **kw)), funcs)
+    else:
+        raise ValueError('Composition of empty sequence not supported.')
+
+def letterbox_image(image, size):
+    '''resize image with unchanged aspect ratio using padding'''
+    iw, ih = image.size
+    w, h = size
+    scale = min(w/iw, h/ih)
+    nw = int(iw*scale)
+    nh = int(ih*scale)
+
+    image = image.resize((nw,nh), Image.BICUBIC)
+    new_image = Image.new('RGB', size, (128,128,128))
+    new_image.paste(image, ((w-nw)//2, (h-nh)//2))
+    return new_image
+
+def rand(a=0, b=1):
+    return np.random.rand()*(b-a) + a
+
+def get_random_data(annotation_line, input_shape, random=True, max_boxes=20, jitter=.3, hue=.1, sat=1.5, val=1.5, proc_img=True):
+    '''random preprocessing for real-time data augmentation'''
+    line = annotation_line.split()
+    image = Image.open(line[0])
+    iw, ih = image.size
+    h, w = input_shape
+    box = np.array([np.array(list(map(int,box.split(',')))) for box in line[1:]])
+
+    if not random:
+        # resize image
+        scale = min(w/iw, h/ih)
+        nw = int(iw*scale)
+        nh = int(ih*scale)
+        dx = (w-nw)//2
+        dy = (h-nh)//2
+        image_data=0
+        if proc_img:
+            image = image.resize((nw,nh), Image.BICUBIC)
+            new_image = Image.new('RGB', (w,h), (128,128,128))
+            new_image.paste(image, (dx, dy))
+            image_data = np.array(new_image)/255.
+
+        # correct boxes
+        box_data = np.zeros((max_boxes,5))
+        if len(box)>0:
+            np.random.shuffle(box)
+            if len(box)>max_boxes: box = box[:max_boxes]
+            box[:, [0,2]] = box[:, [0,2]]*scale + dx
+            box[:, [1,3]] = box[:, [1,3]]*scale + dy
+            box_data[:len(box)] = box
+
+        return image_data, box_data
+
+    # resize image
+    new_ar = w/h * rand(1-jitter,1+jitter)/rand(1-jitter,1+jitter)
+    scale = rand(.25, 2)
+    if new_ar < 1:
+        nh = int(scale*h)
+        nw = int(nh*new_ar)
+    else:
+        nw = int(scale*w)
+        nh = int(nw/new_ar)
+    image = image.resize((nw,nh), Image.BICUBIC)
+
+    # place image
+    dx = int(rand(0, w-nw))
+    dy = int(rand(0, h-nh))
+    new_image = Image.new('RGB', (w,h), (128,128,128))
+    new_image.paste(image, (dx, dy))
+    image = new_image
+
+    # flip image or not
+    flip = rand()<.5
+    if flip: image = image.transpose(Image.FLIP_LEFT_RIGHT)
+
+    # distort image
+    hue = rand(-hue, hue)
+    sat = rand(1, sat) if rand()<.5 else 1/rand(1, sat)
+    val = rand(1, val) if rand()<.5 else 1/rand(1, val)
+    x = rgb_to_hsv(np.array(image)/255.)
+    x[..., 0] += hue
+    x[..., 0][x[..., 0]>1] -= 1
+    x[..., 0][x[..., 0]<0] += 1
+    x[..., 1] *= sat
+    x[..., 2] *= val
+    x[x>1] = 1
+    x[x<0] = 0
+    image_data = hsv_to_rgb(x) # numpy array, 0 to 1
+
+    # correct boxes
+    box_data = np.zeros((max_boxes,5))
+    if len(box)>0:
+        np.random.shuffle(box)
+        box[:, [0,2]] = box[:, [0,2]]*nw/iw + dx
+        box[:, [1,3]] = box[:, [1,3]]*nh/ih + dy
+        if flip: box[:, [0,2]] = w - box[:, [2,0]]
+        box[:, 0:2][box[:, 0:2]<0] = 0
+        box[:, 2][box[:, 2]>w] = w
+        box[:, 3][box[:, 3]>h] = h
+        box_w = box[:, 2] - box[:, 0]
+        box_h = box[:, 3] - box[:, 1]
+        box = box[np.logical_and(box_w>1, box_h>1)] # discard invalid box
+        if len(box)>max_boxes: box = box[:max_boxes]
+        box_data[:len(box)] = box
+
+    return image_data, box_data
+
+"""余弦退火"""
+def cosine_decay_with_warmup(global_step,
+                             learning_rate_base,
+                             total_steps,
+                             warmup_learning_rate=0.0,
+                             warmup_steps=0,
+                             hold_base_rate_steps=0,
+                             min_learn_rate=0,
+                             ):
+    """
+    参数：
+            global_step: 上面定义的Tcur，记录当前执行的步数。
+            learning_rate_base：预先设置的学习率，当warm_up阶段学习率增加到learning_rate_base，就开始学习率下降。
+            total_steps: 是总的训练的步数，等于epoch*sample_count/batch_size,(sample_count是样本总数，epoch是总的循环次数)
+            warmup_learning_rate: 这是warm up阶段线性增长的初始值
+            warmup_steps: warm_up总的需要持续的步数
+            hold_base_rate_steps: 这是可选的参数，即当warm up阶段结束后保持学习率不变，知道hold_base_rate_steps结束后才开始学习率下降
+    """
+    if total_steps < warmup_steps:
+        raise ValueError('total_steps must be larger or equal to '
+                            'warmup_steps.')
+    #这里实现了余弦退火的原理，设置学习率的最小值为0，所以简化了表达式
+    learning_rate = 0.5 * learning_rate_base * (1 + np.cos(np.pi *
+        (global_step - warmup_steps - hold_base_rate_steps) / float(total_steps - warmup_steps - hold_base_rate_steps)))
+    #如果hold_base_rate_steps大于0，表明在warm up结束后学习率在一定步数内保持不变
+    if hold_base_rate_steps > 0:
+        learning_rate = np.where(global_step > warmup_steps + hold_base_rate_steps,
+                                    learning_rate, learning_rate_base)
+    if warmup_steps > 0:
+        if learning_rate_base < warmup_learning_rate:
+            raise ValueError('learning_rate_base must be larger or equal to '
+                                'warmup_learning_rate.')
+        #线性增长的实现
+        slope = (learning_rate_base - warmup_learning_rate) / warmup_steps
+        warmup_rate = slope * global_step + warmup_learning_rate
+        #只有当global_step 仍然处于warm up阶段才会使用线性增长的学习率warmup_rate，否则使用余弦退火的学习率learning_rate
+        learning_rate = np.where(global_step < warmup_steps, warmup_rate,
+                                    learning_rate)
+
+    learning_rate = max(learning_rate,min_learn_rate)
+    return learning_rate
+
+
+class WarmUpCosineDecayScheduler(keras.callbacks.Callback):
+    """
+    继承Callback，实现对学习率的调度
+    """
+    def __init__(self,
+                 learning_rate_base,
+                 total_steps,
+                 global_step_init=0,
+                 warmup_learning_rate=0.0,
+                 warmup_steps=0,
+                 hold_base_rate_steps=0,
+                 min_learn_rate=0,
+                 # interval_epoch代表余弦退火之间的最低点
+                 interval_epoch=[0.05, 0.15, 0.30, 0.50],
+                 verbose=0):
+        super(WarmUpCosineDecayScheduler, self).__init__()
+        # 基础的学习率
+        self.learning_rate_base = learning_rate_base
+        # 热调整参数
+        self.warmup_learning_rate = warmup_learning_rate
+        # 参数显示
+        self.verbose = verbose
+        # learning_rates用于记录每次更新后的学习率，方便图形化观察
+        self.min_learn_rate = min_learn_rate
+        self.learning_rates = []
+
+        self.interval_epoch = interval_epoch
+        # 贯穿全局的步长
+        self.global_step_for_interval = global_step_init
+        # 用于上升的总步长
+        self.warmup_steps_for_interval = warmup_steps
+        # 保持最高峰的总步长
+        self.hold_steps_for_interval = hold_base_rate_steps
+        # 整个训练的总步长
+        self.total_steps_for_interval = total_steps
+
+        self.interval_index = 0
+        # 计算出来两个最低点的间隔
+        self.interval_reset = [self.interval_epoch[0]]
+        for i in range(len(self.interval_epoch)-1):
+            self.interval_reset.append(self.interval_epoch[i+1]-self.interval_epoch[i])
+        self.interval_reset.append(1-self.interval_epoch[-1])
+
+	#更新global_step，并记录当前学习率
+    def on_batch_end(self, batch, logs=None):
+        self.global_step = self.global_step + 1
+        self.global_step_for_interval = self.global_step_for_interval + 1
+        lr = K.get_value(self.model.optimizer.lr)
+        self.learning_rates.append(lr)
+
+	#更新学习率
+    def on_batch_begin(self, batch, logs=None):
+        # 每到一次最低点就重新更新参数
+        if self.global_step_for_interval in [0]+[int(i*self.total_steps_for_interval) for i in self.interval_epoch]:
+            self.total_steps = self.total_steps_for_interval * self.interval_reset[self.interval_index]
+            self.warmup_steps = self.warmup_steps_for_interval * self.interval_reset[self.interval_index]
+            self.hold_base_rate_steps = self.hold_steps_for_interval * self.interval_reset[self.interval_index]
+            self.global_step = 0
+            self.interval_index += 1
+
+        lr = cosine_decay_with_warmup(global_step=self.global_step,
+                                      learning_rate_base=self.learning_rate_base,
+                                      total_steps=self.total_steps,
+                                      warmup_learning_rate=self.warmup_learning_rate,
+                                      warmup_steps=self.warmup_steps,
+                                      hold_base_rate_steps=self.hold_base_rate_steps,
+                                      min_learn_rate = self.min_learn_rate)
+        K.set_value(self.model.optimizer.lr, lr)
+        if self.verbose > 0:
+            print('\nBatch %05d: setting learning '
+                  'rate to %s.' % (self.global_step + 1, lr))
